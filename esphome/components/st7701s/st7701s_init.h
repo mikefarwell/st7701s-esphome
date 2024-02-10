76b0ee2043314f6e87bac0a4b66294d366ed9202
Mike Farwell <mikefarwell@gmail.com>
2024-02-10 15:06:00 -0600

    Create st7701s_init.h

---
+++ esphome/components/st7701s/st7701s_init.h
@@ -0,0 +1,321 @@
+#pragma once
+#include "esphome/core/helpers.h"
+
+#include <cinttypes>
+
+namespace esphome {
+namespace st7701s {
+
+// clang-format off
+static const uint8_t PROGMEM INITCMD_M5STACK[] = {
+  0xEF, 3, 0x03, 0x80, 0x02,
+  0xCF, 3, 0x00, 0xC1, 0x30,
+  0xED, 4, 0x64, 0x03, 0x12, 0x81,
+  0xE8, 3, 0x85, 0x00, 0x78,
+  0xCB, 5, 0x39, 0x2C, 0x00, 0x34, 0x02,
+  0xF7, 1, 0x20,
+  0xEA, 2, 0x00, 0x00,
+  ST7701S_PWCTR1  , 1, 0x23,             // Power control VRH[5:0]
+  ST7701S_PWCTR2  , 1, 0x10,             // Power control SAP[2:0];BT[3:0]
+  ST7701S_VMCTR1  , 2, 0x3e, 0x28,       // VCM control
+  ST7701S_VMCTR2  , 1, 0x86,             // VCM control2
+  ST7701S_MADCTL  , 1, MADCTL_BGR,       // Memory Access Control
+  ST7701S_VSCRSADD, 1, 0x00,             // Vertical scroll zero
+  ST7701S_PIXFMT  , 1, 0x55,
+  ST7701S_FRMCTR1 , 2, 0x00, 0x13,
+  ST7701S_DFUNCTR , 3, 0x08, 0x82, 0x27, // Display Function Control
+  0xF2, 1, 0x00,                         // 3Gamma Function Disable
+  ST7701S_GAMMASET , 1, 0x01,             // Gamma curve selected
+  ST7701S_GMCTRP1 , 15, 0x0F, 0x31, 0x2B, 0x0C, 0x0E, 0x08, // Set Gamma
+                        0x4E, 0xF1, 0x37, 0x07, 0x10, 0x03,
+                        0x0E, 0x09, 0x00,
+  ST7701S_GMCTRN1 , 15, 0x00, 0x0E, 0x14, 0x03, 0x11, 0x07, // Set Gamma
+                        0x31, 0xC1, 0x48, 0x08, 0x0F, 0x0C,
+                        0x31, 0x36, 0x0F,
+  ST7701S_SLPOUT  , 0x80,                // Exit Sleep
+  ST7701S_DISPON  , 0x80,                // Display on
+  0x00                                   // End of list
+};
+
+static const uint8_t PROGMEM INITCMD_M5CORE[] = {
+  ST7701S_SETEXTC, 3, 0xFF,0x93,0x42,   // Turn on the external command
+  ST7701S_PWCTR1 , 2, 0x12, 0x12,
+  ST7701S_PWCTR2 , 1, 0x03,
+  ST7701S_VMCTR1 , 1, 0xF2,
+  ST7701S_IFMODE , 1, 0xE0,
+  0xF6           , 3, 0x01, 0x00, 0x00,
+  ST7701S_GMCTRP1,15, 0x00,0x0C,0x11,0x04,0x11,0x08,0x37,0x89,0x4C,0x06,0x0C,0x0A,0x2E,0x34,0x0F,
+  ST7701S_GMCTRN1,15, 0x00,0x0B,0x11,0x05,0x13,0x09,0x33,0x67,0x48,0x07,0x0E,0x0B,0x2E,0x33,0x0F,
+  ST7701S_DFUNCTR, 4, 0x08,0x82,0x1D,0x04,
+  ST7701S_IDMOFF , 0,
+  ST7701S_DISPON , 0x80,                // Display on
+  ST7701S_SLPOUT , 0x80,                // Exit Sleep
+
+  0x00                                   // End of list
+};
+
+
+
+static const uint8_t PROGMEM INITCMD_ILI9341[] = {
+  0xEF, 3, 0x03, 0x80, 0x02,
+  0xCF, 3, 0x00, 0xC1, 0x30,
+  0xED, 4, 0x64, 0x03, 0x12, 0x81,
+  0xE8, 3, 0x85, 0x00, 0x78,
+  0xCB, 5, 0x39, 0x2C, 0x00, 0x34, 0x02,
+  0xF7, 1, 0x20,
+  0xEA, 2, 0x00, 0x00,
+  ST7701S_PWCTR1  , 1, 0x23,             // Power control VRH[5:0]
+  ST7701S_PWCTR2  , 1, 0x10,             // Power control SAP[2:0];BT[3:0]
+  ST7701S_VMCTR1  , 2, 0x3e, 0x28,       // VCM control
+  ST7701S_VMCTR2  , 1, 0x86,             // VCM control2
+  ST7701S_MADCTL  , 1, 0x48,             // Memory Access Control
+  ST7701S_VSCRSADD, 1, 0x00,             // Vertical scroll zero
+  ST7701S_PIXFMT  , 1, 0x55,
+  ST7701S_FRMCTR1 , 2, 0x00, 0x18,
+  ST7701S_DFUNCTR , 3, 0x08, 0x82, 0x27, // Display Function Control
+  0xF2, 1, 0x00,                         // 3Gamma Function Disable
+  ST7701S_GAMMASET , 1, 0x01,             // Gamma curve selected
+  ST7701S_GMCTRP1 , 15, 0x0F, 0x31, 0x2B, 0x0C, 0x0E, 0x08, // Set Gamma
+                        0x4E, 0xF1, 0x37, 0x07, 0x10, 0x03,
+                        0x0E, 0x09, 0x00,
+  ST7701S_GMCTRN1 , 15, 0x00, 0x0E, 0x14, 0x03, 0x11, 0x07, // Set Gamma
+                        0x31, 0xC1, 0x48, 0x08, 0x0F, 0x0C,
+                        0x31, 0x36, 0x0F,
+  ST7701S_SLPOUT  , 0x80,                // Exit Sleep
+  ST7701S_DISPON  , 0x80,                // Display on
+  0x00                                   // End of list
+};
+
+static const uint8_t PROGMEM INITCMD_ILI9481[] = {
+  ST7701S_SLPOUT ,  0x80,    // Exit sleep mode
+  ST7701S_PWSET  , 3, 0x07, 0x41, 0x1D,
+  ST7701S_VMCTR  , 3, 0x00, 0x1C, 0x1F,
+  ST7701S_PWSETN , 2, 0x01, 0x11,
+  ST7701S_PWCTR1 , 5, 0x10, 0x3B, 0x00, 0x02, 0x11,
+  ST7701S_VMCTR1 , 1, 0x03,
+  ST7701S_IFCTR  , 1, 0x83,
+  ST7701S_GMCTR  ,12, 0x00, 0x26, 0x21, 0x00, 0x00, 0x1F, 0x65, 0x23, 0x77, 0x00, 0x0F, 0x00,
+  ST7701S_IFMODE , 1, 0x00,  // CommandAccessProtect
+  ST7701S_PTLAR , 4, 0, 0, 1, 0xDF,
+  0xE4        , 1, 0xA0,
+  ST7701S_MADCTL  , 1, MADCTL_MV | MADCTL_BGR,       // Memory Access Control
+  ST7701S_CSCON , 1, 0x01,
+  ST7701S_PIXFMT, 1, 0x55,  // 16 bit mode
+  ST7701S_INVON, 0,
+  ST7701S_DISPON, 0x80,     // Set display on
+  0x00 // end
+};
+
+static const uint8_t PROGMEM INITCMD_ILI9481_18[] = {
+    ST7701S_SLPOUT ,  0x80,    // Exit sleep mode
+    ST7701S_PWSET  , 3, 0x07, 0x41, 0x1D,
+    ST7701S_VMCTR  , 3, 0x00, 0x1C, 0x1F,
+    ST7701S_PWSETN , 2, 0x01, 0x11,
+    ST7701S_PWCTR1 , 5, 0x10, 0x3B, 0x00, 0x02, 0x11,
+    ST7701S_VMCTR1 , 1, 0x03,
+    ST7701S_IFCTR  , 1, 0x83,
+    ST7701S_GMCTR  ,12, 0x00, 0x26, 0x21, 0x00, 0x00, 0x1F, 0x65, 0x23, 0x77, 0x00, 0x0F, 0x00,
+    ST7701S_IFMODE , 1, 0x00,  // CommandAccessProtect
+    ST7701S_PTLAR , 4, 0, 0, 1, 0xDF,
+    0xE4        , 1, 0xA0,
+    ST7701S_MADCTL  , 1, MADCTL_MX| MADCTL_BGR,       // Memory Access Control
+    ST7701S_CSCON , 1, 0x01,
+    ST7701S_PIXFMT, 1, 0x66,  // 18 bit mode
+    ST7701S_INVON, 0,
+    ST7701S_DISPON, 0x80,     // Set display on
+    0x00 // end
+};
+
+static const uint8_t PROGMEM INITCMD_ILI9486[] = {
+  ST7701S_SLPOUT, 0x80,
+  ST7701S_PIXFMT, 1, 0x55,
+  ST7701S_PWCTR3, 1, 0x44,
+  ST7701S_VMCTR1, 4, 0x00, 0x00, 0x00, 0x00,
+  ST7701S_GMCTRP1, 15, 0x0f,0x1f,0x1c,0x0c,0x0f,0x08,0x48,0x98,0x37,0x0a,0x13,0x04,0x11,0x0d,0x00,
+  ST7701S_GMCTRN1, 15, 0x0f,0x32,0x2e,0x0b,0x0d,0x05,0x47,0x75,0x37,0x06,0x10,0x03,0x24,0x20,0x00,
+  ST7701S_INVOFF, 0x80,
+  ST7701S_MADCTL, 1, 0x48,
+  ST7701S_DISPON, 0x80,
+
+  // ST7701S_MADCTL, 1, MADCTL_BGR | MADCTL_MV, //hardware rotation
+  0x00                                   // End of list
+};
+
+
+static const uint8_t INITCMD_ILI9488[] = {
+  ST7701S_GMCTRP1,15, 0x0f, 0x24, 0x1c, 0x0a, 0x0f, 0x08, 0x43, 0x88, 0x32, 0x0f, 0x10, 0x06, 0x0f, 0x07, 0x00,
+  ST7701S_GMCTRN1,15, 0x0F, 0x38, 0x30, 0x09, 0x0f, 0x0f, 0x4e, 0x77, 0x3c, 0x07, 0x10, 0x05, 0x23, 0x1b, 0x00,
+
+  ST7701S_PWCTR1,  2, 0x17, 0x15,  // VRH1 VRH2
+  ST7701S_PWCTR2,  1, 0x41,  // VGH, VGL
+  ST7701S_VMCTR1,  3, 0x00, 0x12, 0x80,    // nVM VCM_REG VCM_REG_EN
+
+  ST7701S_IFMODE,  1, 0x00,
+  ST7701S_FRMCTR1, 1, 0xA0,  // Frame rate = 60Hz
+  ST7701S_INVCTR,  1, 0x02,  // Display Inversion Control = 2dot
+
+  0xE9, 1, 0x00,   // Set Image Functio. Disable 24 bit data
+
+  ST7701S_ADJCTL3, 4, 0xA9, 0x51, 0x2C, 0x82,  // Adjust Control 3
+  ST7701S_PIXFMT, 1, 0x66,   //ILI9488 only supports 18-bit pixel format in 4/3 wire SPI mode
+  ST7701S_SLPOUT,  0x80,    // Exit sleep mode
+  ST7701S_DISPON,  0x80,    // Set display on
+  0x00 // end
+};
+
+static const uint8_t INITCMD_WAVESHARE_RES_3_5[] = {
+    ST7701S_PWCTR3, 1, 0x33,
+    ST7701S_VMCTR1, 3, 0x00, 0x1e, 0x80,
+    ST7701S_FRMCTR1, 1, 0xA0,
+    ST7701S_GMCTRP1, 15, 0x0, 0x13, 0x18, 0x04, 0x0F, 0x06, 0x3a, 0x56, 0x4d, 0x03, 0x0a, 0x06, 0x30, 0x3e, 0x0f,
+    ST7701S_GMCTRN1, 15, 0x0, 0x13, 0x18, 0x01, 0x11, 0x06, 0x38, 0x34, 0x4d, 0x06, 0x0d, 0x0b, 0x31, 0x37, 0x0f,
+    ST7701S_PIXFMT, 1, 0x55,
+    ST7701S_SLPOUT, 0x80,   // slpout, delay
+    ST7701S_DISPON, 0,
+    0x00                                   // End of list
+};
+
+static const uint8_t PROGMEM INITCMD_ILI9488_A[] = {
+  ST7701S_GMCTRP1,15, 0x00, 0x03, 0x09, 0x08, 0x16, 0x0A, 0x3F, 0x78, 0x4C, 0x09, 0x0A, 0x08, 0x16, 0x1A, 0x0F,
+  ST7701S_GMCTRN1,15, 0x00, 0x16, 0x19, 0x03, 0x0F, 0x05, 0x32, 0x45, 0x46, 0x04, 0x0E, 0x0D, 0x35, 0x37, 0x0F,
+
+  ST7701S_PWCTR1,  2, 0x17, 0x15,  // VRH1 VRH2
+  ST7701S_PWCTR2,  1, 0x41,  // VGH, VGL
+  ST7701S_VMCTR1,  3, 0x00, 0x12, 0x80,    // nVM VCM_REG VCM_REG_EN
+
+  ST7701S_IFMODE,  1, 0x00,
+  ST7701S_FRMCTR1, 1, 0xA0,  // Frame rate = 60Hz
+  ST7701S_INVCTR,  1, 0x02,  // Display Inversion Control = 2dot
+
+  ST7701S_DFUNCTR, 2, 0x02, 0x02, // Nomal scan
+
+  0xE9, 1, 0x00,   // Set Image Functio. Disable 24 bit data
+
+  ST7701S_ADJCTL3, 4, 0xA9, 0x51, 0x2C, 0x82,  // Adjust Control 3
+
+  ST7701S_MADCTL,  1, 0x28,
+  //ST7701S_PIXFMT,  1, 0x55,  // Interface Pixel Format = 16bit
+  ST7701S_PIXFMT, 1, 0x66,   //ILI9488 only supports 18-bit pixel format in 4/3 wire SPI mode
+
+
+
+  // 5 frames
+  //ST7701S_ETMOD,   1, 0xC6,  //
+
+
+  ST7701S_SLPOUT,  0x80,    // Exit sleep mode
+  //ST7701S_INVON  , 0,
+  ST7701S_DISPON,  0x80,    // Set display on
+  0x00 // end
+};
+
+static const uint8_t PROGMEM INITCMD_ST7796[] = {
+  // This ST7796S initilization routine was copied from https://github.com/prenticedavid/Adafruit_ST7796S_kbv/blob/master/Adafruit_ST7796S_kbv.cpp
+  ST7701S_SWRESET, 0x80,         // Soft reset, then delay 150 ms
+  ST7701S_CSCON, 1, 0xC3,              // ?? Unlock Manufacturer
+  ST7701S_CSCON, 1, 0x96,
+  ST7701S_VMCTR1, 1, 0x1C,              //VCOM  Control 1 [1C]
+  ST7701S_MADCTL, 1, 0x48,              //Memory Access [00]
+  ST7701S_PIXFMT, 1, 0x55,              //565
+  ST7701S_IFMODE, 1, 0x80,              //Interface     [00]
+  ST7701S_INVCTR, 1, 0x01,              //Inversion Control [01]
+  ST7701S_DFUNCTR, 3, 0x80, 0x02, 0x3B,  // Display Function Control [80 02 3B] .kbv SS=1, NL=480
+  ST7701S_ETMOD, 1, 0xC6,              //Entry Mode      [06]
+
+  ST7701S_CSCON, 1, 0x69,              //?? lock manufacturer commands
+  ST7701S_CSCON, 1, 0x3C,              //
+  ST7701S_SLPOUT, 0x80, // Exit Sleep, then delay 150 ms
+  ST7701S_DISPON, 0x80, // Main screen turn on, delay 150 ms
+  0x00                                   // End of list
+};
+
+static const uint8_t PROGMEM INITCMD_S3BOX[] = {
+  0xEF, 3, 0x03, 0x80, 0x02,
+  0xCF, 3, 0x00, 0xC1, 0x30,
+  0xED, 4, 0x64, 0x03, 0x12, 0x81,
+  0xE8, 3, 0x85, 0x00, 0x78,
+  0xCB, 5, 0x39, 0x2C, 0x00, 0x34, 0x02,
+  0xF7, 1, 0x20,
+  0xEA, 2, 0x00, 0x00,
+  ST7701S_PWCTR1  , 1, 0x23,             // Power control VRH[5:0]
+  ST7701S_PWCTR2  , 1, 0x10,             // Power control SAP[2:0];BT[3:0]
+  ST7701S_VMCTR1  , 2, 0x3e, 0x28,       // VCM control
+  ST7701S_VMCTR2  , 1, 0x86,             // VCM control2
+  ST7701S_MADCTL  , 1, 0xC8,             // Memory Access Control
+  ST7701S_VSCRSADD, 1, 0x00,             // Vertical scroll zero
+  ST7701S_PIXFMT  , 1, 0x55,
+  ST7701S_FRMCTR1 , 2, 0x00, 0x18,
+  ST7701S_DFUNCTR , 3, 0x08, 0x82, 0x27, // Display Function Control
+  0xF2, 1, 0x00,                         // 3Gamma Function Disable
+  ST7701S_GAMMASET , 1, 0x01,             // Gamma curve selected
+  ST7701S_GMCTRP1 , 15, 0x0F, 0x31, 0x2B, 0x0C, 0x0E, 0x08, // Set Gamma
+                        0x4E, 0xF1, 0x37, 0x07, 0x10, 0x03,
+                        0x0E, 0x09, 0x00,
+  ST7701S_GMCTRN1 , 15, 0x00, 0x0E, 0x14, 0x03, 0x11, 0x07, // Set Gamma
+                        0x31, 0xC1, 0x48, 0x08, 0x0F, 0x0C,
+                        0x31, 0x36, 0x0F,
+  ST7701S_SLPOUT  , 0x80,                // Exit Sleep
+  ST7701S_DISPON  , 0x80,                // Display on
+  0x00                                   // End of list
+};
+
+static const uint8_t PROGMEM INITCMD_S3BOXLITE[] = {
+  0xEF, 3, 0x03, 0x80, 0x02,
+  0xCF, 3, 0x00, 0xC1, 0x30,
+  0xED, 4, 0x64, 0x03, 0x12, 0x81,
+  0xE8, 3, 0x85, 0x00, 0x78,
+  0xCB, 5, 0x39, 0x2C, 0x00, 0x34, 0x02,
+  0xF7, 1, 0x20,
+  0xEA, 2, 0x00, 0x00,
+  ST7701S_PWCTR1  , 1, 0x23,             // Power control VRH[5:0]
+  ST7701S_PWCTR2  , 1, 0x10,             // Power control SAP[2:0];BT[3:0]
+  ST7701S_VMCTR1  , 2, 0x3e, 0x28,       // VCM control
+  ST7701S_VMCTR2  , 1, 0x86,             // VCM control2
+  ST7701S_MADCTL  , 1, 0x40,             // Memory Access Control
+  ST7701S_VSCRSADD, 1, 0x00,             // Vertical scroll zero
+  ST7701S_PIXFMT  , 1, 0x55,
+  ST7701S_FRMCTR1 , 2, 0x00, 0x18,
+  ST7701S_DFUNCTR , 3, 0x08, 0x82, 0x27, // Display Function Control
+  0xF2, 1, 0x00,                         // 3Gamma Function Disable
+  ST7701S_GAMMASET , 1, 0x01,             // Gamma curve selected
+  ST7701S_GMCTRP1 , 14, 0xF0, 0x09, 0x0B, 0x06, 0x04, 0x15, // Set Gamma
+                        0x2F, 0x54, 0x42, 0x3C, 0x17, 0x14,
+                        0x18, 0x1B,
+  ST7701S_GMCTRN1 , 14, 0xE0, 0x09, 0x0B, 0x06, 0x04, 0x03, // Set Gamma
+                        0x2B, 0x43, 0x42, 0x3B, 0x16, 0x14,
+                        0x17, 0x1B,
+  ST7701S_SLPOUT  , 0x80,                // Exit Sleep
+  ST7701S_DISPON  , 0x80,                // Display on
+  0x00                                   // End of list
+};
+
+static const uint8_t PROGMEM INITCMD_ST7789V[] = {
+  ST7701S_SLPOUT  , 0x80,                // Exit Sleep
+  ST7701S_DISPON  , 0x80,                // Display on
+  ST7701S_MADCTL  , 1, 0x08,             // Memory Access Control, BGR
+  ST7701S_DFUNCTR, 2, 0x0A, 0x82,
+  ST7701S_PIXFMT  , 1, 0x55,
+  ST7701S_FRMCTR2, 5, 0x0C, 0x0C, 0x00, 0x33, 0x33,
+  ST7701S_ETMOD, 1, 0x35, 0xBB, 1, 0x28,
+  ST7701S_PWCTR1  , 1, 0x0C,             // Power control VRH[5:0]
+  ST7701S_PWCTR3  , 2, 0x01, 0xFF,
+  ST7701S_PWCTR4  , 1, 0x10,
+  ST7701S_PWCTR5  , 1, 0x20,
+  ST7701S_IFCTR  , 1, 0x0F,
+  ST7701S_PWSET, 2, 0xA4, 0xA1,
+  ST7701S_GMCTRP1 , 14,
+  0xd0, 0x00, 0x02, 0x07, 0x0a,
+  0x28, 0x32, 0x44, 0x42, 0x06, 0x0e,
+  0x12, 0x14, 0x17,
+  ST7701S_GMCTRN1 , 14,
+  0xd0, 0x00, 0x02, 0x07, 0x0a,
+  0x28, 0x31, 0x54, 0x47,
+  0x0e, 0x1c, 0x17, 0x1b,
+  0x1e,
+  ST7701S_DISPON  , 0x80,                // Display on
+  0x00                                   // End of list
+};
+
+// clang-format on
+}  // namespace st7701s
+}  // namespace esphome
