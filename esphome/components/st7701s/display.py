7ba82b0686179d26bbce866c10fd416b97818b46
Mike Farwell <mikefarwell@gmail.com>
2024-02-10 15:24:34 -0600

    Update display.py

--- esphome/components/st7701s/display.py
+++ esphome/components/st7701s/display.py
@@ -91,7 +91,8 @@ def _validate(config):
 
 CONFIG_SCHEMA = cv.All(
     font.validate_pillow_installed,
-    display.FULL_DISPLAY_SCHEMA#.extend(
+    display.FULL_DISPLAY_SCHEMA
+#    .extend(
 #        {
 #            cv.GenerateID(): cv.declare_id(ST7701SDisplay),
 #            cv.Required(CONF_MODEL): cv.enum(MODELS, upper=True, space="_"),
