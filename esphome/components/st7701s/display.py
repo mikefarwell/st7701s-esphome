142d3fe87354adc74833ee40eee22972551b6018
Mike Farwell <mikefarwell@gmail.com>
2024-02-10 16:34:00 -0600

    Update display.py

--- esphome/components/st7701s/display.py
+++ esphome/components/st7701s/display.py
@@ -5,8 +5,6 @@ from esphome.components import display, font
 from esphome.components.display import validate_rotation
 from esphome.core import CORE, HexInt
 from esphome.const import (
-    CONF_COLOR_PALETTE,
-    #CONF_DC_PIN,
     CONF_ID,
     CONF_LAMBDA,
     CONF_MODEL,
@@ -62,19 +60,6 @@ CONF_INVERT_DISPLAY = "invert_display"
 
 
 def _validate(config):
-    if config.get(CONF_COLOR_PALETTE) == "IMAGE_ADAPTIVE" and not config.get(
-        CONF_COLOR_PALETTE_IMAGES
-    ):
-        raise cv.Invalid(
-            "Color palette in IMAGE_ADAPTIVE mode requires at least one 'color_palette_images' entry to generate palette"
-        )
-    if (
-        config.get(CONF_COLOR_PALETTE_IMAGES)
-        and config.get(CONF_COLOR_PALETTE) != "IMAGE_ADAPTIVE"
-    ):
-        raise cv.Invalid(
-            "Providing color palette images requires palette mode to be 'IMAGE_ADAPTIVE'"
-        )
     if CORE.is_esp8266 and config.get(CONF_MODEL) not in [
         "M5STACK",
         "TFT_2.4",
@@ -128,8 +113,6 @@ async def to_code(config):
     var = cg.Pvariable(config[CONF_ID], rhs)
 
     await display.register_display(var, config)
-    #dc = await cg.gpio_pin_expression(config[CONF_DC_PIN])
-    #cg.add(var.set_dc_pin(dc))
     #if CONF_COLOR_ORDER in config:
     #    cg.add(var.set_color_order(COLOR_ORDERS[config[CONF_COLOR_ORDER]]))
     #if CONF_TRANSFORM in config:
@@ -162,47 +145,8 @@ async def to_code(config):
             cg.add(var.set_dimensions(width, height))
 
     rhs = None
-    if config[CONF_COLOR_PALETTE] == "GRAYSCALE":
-        cg.add(var.set_buffer_color_mode(ST7701SColorMode.BITS_8_INDEXED))
-        rhs = []
-        for x in range(256):
-            rhs.extend([HexInt(x), HexInt(x), HexInt(x)])
-    elif config[CONF_COLOR_PALETTE] == "IMAGE_ADAPTIVE":
-        cg.add(var.set_buffer_color_mode(ST7701SColorMode.BITS_8_INDEXED))
-        from PIL import Image
-
-        def load_image(filename):
-            path = CORE.relative_config_path(filename)
-            try:
-                return Image.open(path)
-            except Exception as e:
-                raise core.EsphomeError(f"Could not load image file {path}: {e}")
-
-        # make a wide horizontal combined image.
-        images = [load_image(x) for x in config[CONF_COLOR_PALETTE_IMAGES]]
-        total_width = sum(i.width for i in images)
-        max_height = max(i.height for i in images)
-
-        ref_image = Image.new("RGB", (total_width, max_height))
-        x = 0
-        for i in images:
-            ref_image.paste(i, (x, 0))
-            x = x + i.width
-
-        # reduce the colors on combined image to 256.
-        converted = ref_image.convert("P", palette=Image.Palette.ADAPTIVE, colors=256)
-        # if you want to verify how the images look use
-        # ref_image.save("ref_in.png")
-        # converted.save("ref_out.png")
-        palette = converted.getpalette()
-        assert len(palette) == 256 * 3
-        rhs = palette
-    else:
         cg.add(var.set_buffer_color_mode(ST7701SColorMode.BITS_16))
 
     if rhs is not None:
         prog_arr = cg.progmem_array(config[CONF_RAW_DATA_ID], rhs)
         cg.add(var.set_palette(prog_arr))
-
-    #if CONF_INVERT_COLORS in config:
-    #    cg.add(var.invert_colors(config[CONF_INVERT_COLORS]))
